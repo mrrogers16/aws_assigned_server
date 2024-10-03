9947373644f5a44d98ea316efbd7e6edf5b4f189
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2024-10-03 14:08:10 -0500

    finished .js functions
    pushing to staging to test

--- hw06/hw06.js
+++ hw06/hw06.js
@@ -8,10 +8,49 @@ function generateOddNumbers() {
         odds.push(i);
     }
     // Find the <p> tag with id="odds" and display as text
-    document.getElementById("odds").innerText = odds.join(", " + '.');
+    document.getElementById("odds").innerText = odds.join(", ");
 }
-
+// Call funtion
 generateOddNumbers();
 
+function generateFibNumbers() {
+    console.log("generateFibNumbers is called") // debug line
+    let fibo = [0, 1]; //first 2 fib numbers to get us started 
+    // Calculate next fib number at fib[2]. First 10 fibs = 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+    for (let i = 2; i < 100; i++) {
+        //each new fib number is the sum of the previous two 
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    // Find <p> tag with id="fibo" and display as text
+    document.getElementById("fibo").innerText = fibo.join(", ");
+}
 
 
+function generatePrimeNumbers() {
+    console.log("generatePrimeNumbers was called") // debug line
+    let primes = [] // empty array to store primes
+
+    // loop to check if number is prime from 2 - 99. 1 is not a prime so we skip
+    for (let i = 2; i < 100; i++) {
+        // assume number is prime
+        let isPrime = true;
+        // loop to check if i is divisible by any number between 2 and the sqrt of i
+        // if numbers divisor is larger than its square root we have already found the smaller divisor
+        for (let j = 2; j <= Math.sqrt(i); j++) {
+            // if i and j are divisible isPrime = false
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        // if the isPrime flag hasnt been set to false we add it to the primes array
+        if (isPrime) {
+            primes.push(i);
+        }
+    }
+    // find the <p> tag with id="primes" and display as text 
+    document.getElementById("primes").innerText = primes.join(", ");
+}
+generateOddNumbers();
+generateFibNumbers();
+generatePrimeNumbers();
